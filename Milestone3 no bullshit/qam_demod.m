c65e84af85bfe585e24c8b83b1ff4a3e7c575eb1
Corneel <corny.tjonck@student.kuleuven.be>
2020-12-03 15:36:53 +0100

    Only used files

---
+++ Milestone3 no bullshit/qam_demod.m
@@ -0,0 +1,7 @@
+function demodulatedSequence = qam_demod(sequence, M)
+
+%demodInstance = qamdemod('M', M, 'OutputType', 'bit');
+%demodulatedSequence = demodulate(demodInstance,qamSequence);
+demodulatedSequence  = qamdemod(sequence,M, 'OutputType', 'bit', 'PlotConstellation', false);
+
+end
